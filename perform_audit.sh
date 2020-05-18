c15529152956ca0a7e5cee4d34927a1e52bfd4eb
Jens Brimfors <jens.brimfors@nordnet.se>
2020-05-18 13:26:49 +0200

    Add output about audit size

--- perform_audit.sh
+++ perform_audit.sh
@@ -2,10 +2,12 @@
 
 cargo install -q cargo-edit --version 0.6.0 || echo "cargo-edit already installed"
 cargo install -q cargo-audit --version 0.12.0 || echo "cargo-audit already installed"
-
-for page in $(seq 1 5)
+pages=5
+page_size=50
+total_size=$(expr $pages \* $page_size)
+for page in $(seq 1 $pages)
 do
-  URL="https://crates.io/api/v1/crates?page=${page}&per_page=50&sort=recent-downloads"
+  URL="https://crates.io/api/v1/crates?page=${page}&per_page=$page_size&sort=recent-downloads"
   echo "Fetching page $page with url $URL"
   for crate in $(curl $URL | jq -r ".crates[] | .id"   )
   do
@@ -16,6 +18,7 @@ done
 cat README_TEMPLATE.md > README.md
 echo "" >> README.md
 echo "Last run:   $(date)" >> README.md
+echo "Audited the top $total_size crates from crates.io" >> README.md
 echo "" >> README.md
 echo "----" >> README.md
 echo "" >> README.md
