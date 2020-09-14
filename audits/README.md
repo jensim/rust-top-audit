998f6fa09e3043f6c24559d37c1f5fa1b292412b
Jens Brimfors <jens.brimfors@nordnet.se>
2020-09-14 06:20:28 +0200

    Start reworking the script to a per-crate-audit-setup

---
+++ audits/README.md
@@ -0,0 +1,2 @@
+This folder contains audited projects  
+These projects are generated during build, and the ones without errors or warnings are removed  
--- perform_audit.sh
+++ perform_audit.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+set -e
+set -x
+
 pages=20
 page_size=50
 total_size=$(expr $pages \* $page_size)
 failed_fetch=()
+processes=()
+top_crates_file='/tmp/rust_top_audit/crates.txt'
 
-git pull --rebase --ff-only
+#git pull --rebase --ff-only --autostash
 cat README_TEMPLATE.md > README.md
 {
   echo ""
@@ -16,90 +21,42 @@ cat README_TEMPLATE.md > README.md
   echo "----"
 } >> README.md
 
-rm Cargo* || echo "" > /dev/null
-cargo init
 cargo install -q cargo-edit --version 0.6.0 > /dev/null 2>&1 || echo "cargo-edit already installed"
 cargo install -q cargo-audit --version 0.12.0 > /dev/null 2>&1 || echo "cargo-audit already installed"
 
-for page in $(seq 1 $pages)
-do
-  URL="https://crates.io/api/v1/crates?page=${page}&per_page=$page_size&sort=recent-downloads"
-  echo "Fetching page $page with url $URL"
-  for crate in $(curl -s $URL | jq -r ".crates[] | .id"   )
-  do
-    if cargo add "$crate" > /dev/null 2>&1 ; then
-      echo "" > /dev/null
-    else
-      failed_fetch+=("$crate")
-    fi
+function wait_and_reset_process_queue() {
+  for p in ${processes[@]} ; do
+    wait $p
   done
-done
+  processes=()
+}
 
-if [[ "${#failed_fetch[@]}" != 0 ]] ; then
-  {
-    echo ""
-    echo "## Failed fetching crates:"
-    echo ""
-    for failed in "${failed_fetch[@]}" ; do
-      echo " * [${failed}](https://crates.io/crates/${failed})"
-    done
-  } >> README.md
+if ! [ -f "$top_crates_file" ] ; then
+  for page in $(seq 1 $pages) ; do
+    (
+      URL="https://crates.io/api/v1/crates?page=${page}&per_page=$page_size&sort=recent-downloads"
+      echo "Fetching page $page with url $URL"
+      counter=$(((page-1) * page_size))
+      for c in $(curl -q -f -s $URL | jq -r '.crates[] | .id') ; do
+        let "conter++"
+        padded_counter=$(printf "%05d", $counter)
+        echo "$padded_counter $c" >> "$top_crates_file"
+      done
+    ) &
+    processes+=($!)
+  done
+  wait_and_reset_process_queue()
 fi
 
-rm Cargo.lock 2>/dev/null
 
-dropped_crates=()
-cargo_audit_output=$(cargo audit --color never 2>&1)
-while echo "$cargo_audit_output" | grep '... which is depended on by'
-do
-  echo "Will try removing conflicting crates.."
-  extra_crate="$(echo "$cargo_audit_output" 2>&1 |
-    # cargo audit --color never 2>&1 |
-    grep '... which is depended on by ' |
-    sed -n '/)/q;p' |
-    tail -1 |
-    sed 's/^.*which is depended on by `//g' |
-    sed 's/ .*$//g')"
-  cause_crate="$(echo "$cargo_audit_output" 2>&1 |
-    # cargo audit --color never 2>&1 |
-    grep 'error: failed to select a version for' |
-    head -1 |
-    sed 's/error: failed to select a version for `//g' |
-    sed 's/`\.$//g')"
-  dropped_crates+=("[${extra_crate}](https://crates.io/crates/${extra_crate}) ([${cause_crate}](https://crates.io/crates/${cause_crate}))")
-  if ! cargo rm "$extra_crate" ; then
-    echo "Failed removing conflict crate that was preventing version resolve ($extra_crate)"
-    exit 1
-  fi
-  cargo_audit_output=$(cargo audit --color never 2>&1)
-done
 
-if [ "${#dropped_crates[@]}" -gt 0 ]; then
-  {
-    echo ""
-    echo "# Dropped crates due to version conflicts:"
-    echo "(These are probably lagging in dependency version, might be transitive)"
-    echo ""
-    for crate_link in "${dropped_crates[@]}" ; do
-      echo "* ${crate_link}"
-    done
-  } >> README.md
-fi
-
-{
-  echo ""
-  echo "## Audit"
-  echo ""
-  echo "\`\`\`"
-  echo "$cargo_audit_output"
-  echo "\`\`\`"
-  echo ""
-} >> README.md
-
-if [ "$#" -gt 0 ] ; then
+# # # # # # # # # # # # # # # # #
+# If running on GitHub, push it #
+# # # # # # # # # # # # # # # # #
+if [ "$GITHUB_ACTOR" != '' && "$GITHUB_TOKEN" != '' && "$GITHUB_REPOSITORY" != '' && "$GITHUB_REF" != '' ] ; then
   echo "Command was run with trailing garble, that means GO ahead for commit&push to github."
-  git config --global user.email "jens.brimfors@gmail.com"
-  git config --global user.name "GitHub Actions on behalf of Jens Brimfors"
+  git config user.email "jens.brimfors@gmail.com"
+  git config user.name "GitHub Actions on behalf of Jens Brimfors"
   git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
   git pull github "${GITHUB_REF}" --ff-only
 
@@ -109,6 +66,6 @@ if [ "$#" -gt 0 ] ; then
   git commit -m "Update README with audit info"
   git push github "HEAD:${GITHUB_REF}"
 else
-  echo "Wont do any of the git-stuff unless you add some garble args to the script"
+  echo "Wont do any of the git-stuff. You need some env vars set for that to work properly."
   exit 1
 fi
